e97e122b354d802275469ac74a579c0c1d360e34
Queror254 <mwendavictorm@gmail.com>
2024-03-16 23:39:48 +0300

    -new--web--

---
+++ js/script.js
@@ -0,0 +1,69 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu");
+const navClose = document.getElementById("nav-close");
+const navToggle = document.getElementById("nav-toggle");
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+/*=============== REMOVE MENU MOBILE ===============*/
+
+/*=============== HOME SWIPER ===============*/
+var homeSwiper = new Swiper(".home-swiper", {
+  spaceBetween: 30,
+  grubCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+/*=============== DEALS TAB ===============*/
+const tabs = document.querySelectorAll("[data-target]");
+const tabContents = document.querySelectorAll("[content]");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
+
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("active-tab");
+    });
+
+    target.classList.add("active-tab");
+    tabs.forEach((tab) => {
+      tab.classList.remove("active-tab");
+    });
+    tab.classList.add("active-tab");
+  });
+});
+
+/*=============== SHOW SCROLL UP ===============*/
+function scrollUp() {
+  const scrollup = document.getElementById("scroll-up");
+
+  if (this.scrollY >= 300) {
+    scrollup.classList.add("show-scroll");
+  } else {
+    scrollup.classList.remove("show-scroll");
+  }
+}
+
+window.addEventListener("scroll", scrollUp);
